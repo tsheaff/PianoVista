08ef7ded11ce60e35f817b46b4d21b885fce1df5
Tyler Sheaffer <ty.sheaffer@gmail.com>
2020-05-03 18:46:50 -0700

    make the main VC a big black box

--- PianoVista/ViewController.swift
+++ PianoVista/ViewController.swift
@@ -3,13 +3,18 @@ import Cocoa
 class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.wantsLayer = true
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+
+    override func awakeFromNib() {
+        guard let layer = self.view.layer else { return }
+
+        layer.backgroundColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
     }
 }
