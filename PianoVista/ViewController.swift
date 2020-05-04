e7d71b8cdfb42b538e06bb698d9309bf09f9c3a7
Tyler Sheaffer <ty.sheaffer@gmail.com>
2020-05-03 19:35:51 -0700

    listen for note on commands, and call an empty method when they occur

--- PianoVista/ViewController.swift
+++ PianoVista/ViewController.swift
@@ -1,9 +1,12 @@
 import Cocoa
+import MIKMIDI
 
 class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.wantsLayer = true
+
+        listenForMidiEventsFromFirstSource()
     }
 
     override var representedObject: Any? {
@@ -16,5 +19,26 @@ class ViewController: NSViewController {
         guard let layer = self.view.layer else { return }
 
         layer.backgroundColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+
+    private func listenForMidiEventsFromFirstSource() {
+        guard let firstSource = MIKMIDIDeviceManager.shared.virtualSources.first else { return }
+        _ = try? MIKMIDIDeviceManager.shared.connectInput(firstSource) { (endpoint, commands) in
+            commands.forEach { [weak self] (command) in
+                self?.handleMidiCommand(command)
+            }
+        }
+    }
+
+    private func handleMidiCommand(_ command: MIKMIDICommand) {
+        if command.commandType == .noteOn {
+            if let noteOnCommand = command as? MIKMIDINoteOnCommand {
+                createColoredCircleForNoteOn(noteOnCommand)
+            }
+        }
+    }
+
+    private func createColoredCircleForNoteOn(_ noteOnCommand: MIKMIDINoteOnCommand) {
+
     }
 }
