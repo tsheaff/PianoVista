8d519b182989af6acd35fa436779d6bdd501d4fe
Tyler Sheaffer <ty.sheaffer@gmail.com>
2020-05-03 17:07:27 -0700

    Initial Commit

---
+++ PianoVista/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PianoVista
+//
+//  Created by Tyler Sheaffer on 5/3/20.
+//  Copyright © 2020 Tyler Sheaffer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
