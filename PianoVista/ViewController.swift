d49407b198beccc4e90a0cf9390d1d86c32614ba
tsheaff <ty.sheaffer@gmail.com>
2020-05-03 22:49:46 -0700

    add some fun flare with a better pallette and hopping ball animations

--- PianoVista/ViewController.swift
+++ PianoVista/ViewController.swift
@@ -31,7 +31,9 @@ class ViewController: NSViewController {
     private func handleMidiCommand(_ command: MIKMIDICommand) {
         if command.commandType == .noteOn {
             if let noteOnCommand = command as? MIKMIDINoteOnCommand {
-                createColoredCircleForNoteOn(noteOnCommand)
+                if noteOnCommand.velocity > 0 {
+                    createColoredCircleForNoteOn(noteOnCommand)
+                }
             }
         }
     }
@@ -40,33 +42,61 @@ class ViewController: NSViewController {
 
     private func frameForNote(_ note: MIKMIDINoteOnCommand) -> CGRect {
         // piano notes range 21 -> 108
-        let dimension = 35 + CGFloat(note.velocity)
+        let dimension = 35 + 1.5 * CGFloat(note.velocity)
         let minNote: UInt = 21
         let maxNote: UInt = 108
         let portionInPianoRange = CGFloat(max(note.note, minNote) - minNote) / CGFloat(maxNote - minNote)
         let centerX = portionInPianoRange * view.frame.size.width
-        let centerY = CGFloat.random(in: 0 ... view.frame.size.height)
+        let centerY = 0.5 * view.frame.size.height  // CGFloat.random(in: 0 ... view.frame.size.height)
         return CGRect(x: centerX - 0.5 * dimension, y: centerY - 0.5 * dimension, width: dimension, height: dimension)
     }
 
     private func createColoredCircleForNoteOn(_ noteOnCommand: MIKMIDINoteOnCommand) {
         let circleLayer = CAShapeLayer()
         let frame = frameForNote(noteOnCommand)
-        circleLayer.path = NSBezierPath(ovalIn: frame).cgPath
+        circleLayer.frame = frame
+        circleLayer.path = NSBezierPath(ovalIn: CGRect(x: 0, y: 0, width: frame.width, height: frame.height)).cgPath
         circleLayer.fillColor = ColorPallette.randomColor()
         self.view.layer?.addSublayer(circleLayer)
 
         let layerKey = UUID().uuidString
         circleLayersByKey[layerKey] = circleLayer
 
+        let scaleAnimation = CAKeyframeAnimation(keyPath: "transform")
+        scaleAnimation.keyTimes = [
+            0.0,
+            0.1,
+            1.0,
+        ]
+
+        let normalizedVelocity = Double(noteOnCommand.velocity) / 127.0
+        let bumpUpY = 1000 * pow(normalizedVelocity, 3.0)
+        let fallBelowY = -1000
+        var maxTransform = CATransform3DScale(circleLayer.transform, 1.1, 1.1, 1.0)
+        maxTransform = CATransform3DTranslate(maxTransform, 0, CGFloat(bumpUpY), 0)
+        var minTransform = CATransform3DScale(circleLayer.transform, 0.6, 0.6, 1.0)
+        minTransform = CATransform3DTranslate(minTransform, 0, CGFloat(fallBelowY), 0)
+        scaleAnimation.values = [
+            NSValue(caTransform3D: circleLayer.transform),
+            NSValue(caTransform3D: maxTransform),
+            NSValue(caTransform3D: minTransform),
+        ]
+        scaleAnimation.timingFunctions = [
+            CAMediaTimingFunction(name: .easeOut),
+            CAMediaTimingFunction(name: .easeIn),
+        ]
+        scaleAnimation.duration = 1.5
+        circleLayer.add(scaleAnimation, forKey: scaleAnimation.keyPath)
+        circleLayer.transform = minTransform
+
         let fadeAnimation = CABasicAnimation(keyPath: "opacity")
         fadeAnimation.setValue(layerKey, forKey: "circleLayerKey")
         fadeAnimation.fromValue = 1.0
         fadeAnimation.toValue = 0.0
-        fadeAnimation.duration = 1
+        fadeAnimation.duration = 1.75 * scaleAnimation.duration
         fadeAnimation.delegate = self
-        circleLayer.add(fadeAnimation, forKey: layerKey)
-        circleLayer.opacity = 0
+        circleLayer.add(fadeAnimation, forKey: fadeAnimation.keyPath)
+        circleLayer.opacity = 0.0
     }
 }
 
@@ -80,13 +110,14 @@ extension ViewController: CAAnimationDelegate {
 }
 
 struct ColorPallette {
-    static var color0 = CGColor.fromRGB(0x511845)
-    static var color1 = CGColor.fromRGB(0x900c3f)
-    static var color2 = CGColor.fromRGB(0xc70039)
-    static var color3 = CGColor.fromRGB(0xff5733)
+    static var color0 = CGColor.fromRGB(0x390099)
+    static var color1 = CGColor.fromRGB(0x9e0059)
+    static var color2 = CGColor.fromRGB(0xff0054)
+    static var color3 = CGColor.fromRGB(0xff5400)
+    static var color4 = CGColor.fromRGB(0xffbd00)
 
     static var colors: [CGColor] {
-        return [color0, color1, color2, color3]
+        return [color0, color1, color2, color3, color4]
     }
 
     static func randomColor() -> CGColor {
